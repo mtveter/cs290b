f860e038d71b7a956fcaaf7881fb413a4105867b
janzh <erikgunnar.jansen@hotmail.com>
2015-04-07 22:19:29 +0200

    Fixed computer interface variables and add implementation to computerimpl

--- homework1/src/api/Computer.java
+++ homework1/src/api/Computer.java
@@ -5,5 +5,8 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface Computer extends Remote, Serializable{
+	public static String SERVICE_NAME = "Computer";
+	public static int PORT = 1099;
+	
 	public <T> T execute(Task<T> task) throws RemoteException;
 }
--- homework1/src/computer/ComputerImpl.java
+++ homework1/src/computer/ComputerImpl.java
@@ -1,5 +1,16 @@
 package computer;
 
-public class ComputerImpl {
+import java.rmi.RemoteException;
+
+import api.Computer;
+import api.Task;
+
+public class ComputerImpl implements Computer{
+
+	@Override
+	public <T> T execute(Task<T> task) throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
