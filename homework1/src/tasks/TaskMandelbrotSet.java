206cc54afae0ee1de559ed964464ab1e79d4ed0a
Vikas Gupta <gvikas91@gmail.com>
2015-04-07 14:28:57 -0700

    Implemented TaskMandelbrotSet

--- homework1/src/tasks/TaskMandelbrotSet.java
+++ homework1/src/tasks/TaskMandelbrotSet.java
@@ -3,16 +3,45 @@ package tasks;
 import api.Task;
 
 public class TaskMandelbrotSet implements Task<Integer[][]>{
+	
+	private double lowerLeftX; 
+	private double lowerLeftY; 
+	private double edgeLength; 
+	private int n;
+	private int iterationLimit; 
+	
 
 	public TaskMandelbrotSet(double lowerLeftX, double lowerLeftY,
 			double edgeLength, int nPixels, int iterationLimit) {
-		// TODO Auto-generated constructor stub
+		this.lowerLeftX = lowerLeftX;
+		this.lowerLeftY = lowerLeftY;
+		this.edgeLength = edgeLength;
+		this.n = nPixels;
+		this.iterationLimit = iterationLimit;
 	}
+
 
 	@Override
 	public Integer[][] execute() {
-		// TODO Auto-generated method stub
-		return null;
+		Integer count[][] = new Integer[n][n];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				double x0 = lowerLeftX + (edgeLength/n)*i; 
+				double y0 = lowerLeftY + (edgeLength/n)*j;
+				double zReal = x0;
+				double zImaginary = y0;
+				int k=0; 
+				while((zReal*zReal - zImaginary*zImaginary) < 4 && k < iterationLimit){
+					double xtemp = zReal*zReal - zImaginary*zImaginary + x0;
+					double ytemp = 2*zReal*zImaginary + y0;
+					zReal = xtemp;
+					zImaginary = ytemp;
+					k++;
+				}
+				count[i][n-j-1] = k; 
+			}
+		}
+		return count;
 	}
 
 }
