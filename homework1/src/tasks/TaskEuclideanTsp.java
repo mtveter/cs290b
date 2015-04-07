a50cb69829a8341b22d9587801eee893512d84ab
janzh <erikgunnar.jansen@hotmail.com>
2015-04-07 22:25:03 +0200

    Added implementations and constructor to remove errors

--- homework1/src/tasks/TaskEuclideanTsp.java
+++ homework1/src/tasks/TaskEuclideanTsp.java
@@ -1,5 +1,20 @@
 package tasks;
 
-public class TaskEuclideanTsp {
+import java.util.List;
+
+import api.Task;
+
+public class TaskEuclideanTsp implements Task<List<Integer>>{
+
+	public TaskEuclideanTsp(double[][] cities) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public List<Integer> execute() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 
 }
--- homework1/src/tasks/TaskMandelbrotSet.java
+++ homework1/src/tasks/TaskMandelbrotSet.java
@@ -1,5 +1,18 @@
 package tasks;
 
-public class TaskMandelbrotSet {
+import api.Task;
+
+public class TaskMandelbrotSet implements Task<Integer[][]>{
+
+	public TaskMandelbrotSet(double lowerLeftX, double lowerLeftY,
+			double edgeLength, int nPixels, int iterationLimit) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Integer[][] execute() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
