62fe5b353f59bf4b13d7f5c6c8917edccada9ffe
janzh <erikgunnar.jansen@hotmail.com>
2015-04-08 20:09:17 +0200

    Implemented computer server impl

--- homework1/src/computer/ComputerImpl.java
+++ homework1/src/computer/ComputerImpl.java
@@ -1,6 +1,8 @@
 package computer;
 
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 
 import api.Computer;
@@ -8,7 +10,7 @@ import api.Task;
 
 public class ComputerImpl extends UnicastRemoteObject implements Computer{
 
-	protected ComputerImpl() throws RemoteException {
+	public ComputerImpl() throws RemoteException {
 		super();
 		// TODO Auto-generated constructor stub
 	}
@@ -16,7 +18,24 @@ public class ComputerImpl extends UnicastRemoteObject implements Computer{
 	@Override
 	public <T> T execute(Task<T> task) throws RemoteException {
 		// TODO Auto-generated method stub
-		return null;
+		return task.execute();
 	}
+	
+	public static void main(String[] args) throws Exception
+    {
+        // construct & set a security manager (unnecessary in this case)
+        System.setSecurityManager( new SecurityManager() );
 
+        // instantiate a server object
+        ComputerImpl computer = new ComputerImpl(); // can throw RemoteException
+
+        // construct an rmiregistry within this JVM using the default port
+        Registry registry = LocateRegistry.createRegistry( Computer.PORT  );
+
+        // bind server in rmiregistry. Can throw exceptions. See api.
+        registry.rebind( Computer.SERVICE_NAME, computer );
+
+        System.out.println("Computer Server: Ready. on port " + Computer.PORT);
+    
+    }
 }
