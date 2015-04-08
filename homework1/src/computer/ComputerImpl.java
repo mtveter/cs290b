5386622e511c4ef55e211c2b99bf91e1cb17327f
janzh <erikgunnar.jansen@hotmail.com>
2015-04-08 19:56:44 +0200

    Added parent class

--- homework1/src/computer/ComputerImpl.java
+++ homework1/src/computer/ComputerImpl.java
@@ -1,11 +1,17 @@
 package computer;
 
 import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
 
 import api.Computer;
 import api.Task;
 
-public class ComputerImpl implements Computer{
+public class ComputerImpl extends UnicastRemoteObject implements Computer{
+
+	protected ComputerImpl() throws RemoteException {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	@Override
 	public <T> T execute(Task<T> task) throws RemoteException {
