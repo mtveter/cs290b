a4cad4ec0cda88f0259350758610ad771e8716d9
janzh <erikgunnar.jansen@hotmail.com>
2015-04-13 20:33:21 +0200

    Serial ID

--- homework2/src/system/ComputerImpl.java
+++ homework2/src/system/ComputerImpl.java
@@ -5,6 +5,11 @@ import java.rmi.server.UnicastRemoteObject;
 
 public class ComputerImpl extends UnicastRemoteObject implements Computer{
 
+	/**
+	 * Generated Serial ID
+	 */
+	private static final long serialVersionUID = 8694827504541478153L;
+
 	protected ComputerImpl() throws RemoteException {
 		super();
 	}
