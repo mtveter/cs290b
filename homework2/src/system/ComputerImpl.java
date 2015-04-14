f3059a2b980d68cc38a387ef5338073786cf2cd9
janzh <erikgunnar.jansen@hotmail.com>
2015-04-14 18:30:29 +0200

    Completed main method of ComputerImpl

--- homework2/src/system/ComputerImpl.java
+++ homework2/src/system/ComputerImpl.java
@@ -1,8 +1,12 @@
 package system;
 
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
+import api.Space;
 import api.Task;
 
 public class ComputerImpl extends UnicastRemoteObject implements Computer{
@@ -23,7 +27,31 @@ public class ComputerImpl extends UnicastRemoteObject implements Computer{
 	}
 	
 	public static void main(String[] args) {
+		// If no argument is passed, then connect to local host, otherwise to IPv4 specified 
+		String domainName;
+		if(args.length > 0){
+			domainName = args[0];
+		}
+		else{domainName = "localhost";}
 		
+		System.setSecurityManager( new SecurityManager() );
+		
+		// Get url of remote space
+		String url = "rmi://" + domainName + ":" + Space.PORT + "/" + Space.SERVICE_NAME;
+		// Try to get remote reference from rmiregistry and register new computer to space
+		try {
+			Space space = (Space) Naming.lookup( url );
+			Computer computer = new ComputerImpl();
+			space.register(computer);
+		}catch (  MalformedURLException malformedException) {
+		    System.err.println("Bad URL: " + malformedException);
+		  }
+		catch (  NotBoundException notBoundException) {
+		    System.err.println("Not Bound: " + notBoundException);
+		  }
+		catch (  RemoteException remoteException) {
+		    System.err.println("Remote Exception: " + remoteException);
+		  }
 	}
 
 }
