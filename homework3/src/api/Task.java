5a4a41d8d131f21b8b52a0b83b3e8551946fc2a9
janzh <erikgunnar.jansen@hotmail.com>
2015-04-24 22:53:05 +0200

    Cleaned up: Task

--- homework3/src/api/Task.java
+++ homework3/src/api/Task.java
@@ -2,10 +2,19 @@ package api;
 
 import java.io.Serializable;
 import java.rmi.RemoteException;
-
+/**
+ * Task interface for implementing Task objects
+ * @param <T>
+ */
 public interface Task<T> extends Serializable{
-	
+	/**
+	 * @return 					Object to call on
+	 * @throws RemoteException	Communication-related exception that may occur during the execution of a remote method call
+	 */
 	public <V> V call() throws RemoteException;
-	
-	public String getId(); 
+	/**
+	 * @return 					Identifier of Task
+	 * @throws RemoteException 	Communication-related exception that may occur during the execution of a remote method call
+	 */
+	public String getId() throws RemoteException ; 
 }
